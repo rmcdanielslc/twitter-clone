d008eff8e0e3b8592684867aed2bbe25d80d5704
rmcdanielslc <rmcdnaielslc@gmail.com>
2017-02-08 22:09:11 -0700

    2/6/17

--- js/animations.js
+++ js/animations.js
@@ -1,8 +1,12 @@
-$('.tweet-compose').on('click', function(){
+
+  $('.tweet-compose').on('click', function(){
   $('#tweet-controls').css('visibility','visible');
   $('#char-count').css('visibility','visible');
-
 });
+
+  $('.tweet-compose').on('click', function(){
+  $(this).css('height', '150px')
+  });
 
 var maxLength = 140;
 $('textarea').keyup(function(){
@@ -21,7 +25,33 @@ $('textarea').keyup(function(){
 });
 
 $('#tweet-submit').on('click',function(){
-  var value = $('.tweet-compose').val();
-  var content = $('#stream');
-  $(content).prepend(value);
-})
+  var tweetText = $('.tweet-compose').val();
+  var newRyanTweet = $('.tweet').first().clone();
+
+  newRyanTweet.find('.avatar').attr('src', 'img/alagoon.jpg');
+  newRyanTweet.find('.fullname').html('Ryan McDaniel');
+  newRyanTweet.find('.username').html('@slcrymick');
+  newRyanTweet.find('.tweet-text').html(tweetText);
+  newRyanTweet.find('.num-retweets').html(0);
+  newRyanTweet.find('.num-favorites').html(0);
+  newRyanTweet.find('tweet-compose').html('reply to @slcrymick');
+  newRyanTweet.find('.users-interact').html('<div></div>');
+  $('#stream').prepend(newRyanTweet);
+});
+
+$('#tweet-submit').on('click',function(){
+  $('.tweet-compose').val('');
+  $('#char-count').html(lengthAllowed);
+  $('#tweet-controls').hide();
+  $('.tweet-compose').css('height', '60px');
+});
+
+$('.tweet').on('mouseover', function(){
+  $(this).find('.tweet-actions').show();
+  $(this).find('.stats').show();
+});
+$('.tweet').on('mouseout', function() {
+    $(this).find('.tweet-actions').hide();
+    $(this).find('.stats').hide();
+});
+$('.stats').hide();
